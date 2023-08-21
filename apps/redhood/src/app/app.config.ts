8ab6f6da1e1634eaafa603eba6e30a7771a81812
Red Hood <thilakshanlk@gmail.com>
2023-08-22 00:48:00 +0530

    add PWA packages

--- apps/redhood/src/app/app.config.ts
+++ apps/redhood/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
@@ -7,15 +7,15 @@ import { appRoutes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { NgxsModule } from '@ngxs/store';
 import { AppState } from './states/app/app.state';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes,
-     withEnabledBlockingInitialNavigation()),
-      provideAnimations(),
-      importProvidersFrom(
-        NgxsModule.forRoot([AppState], {
-      developmentMode: true,
-    })
-      ),
-    ],
+  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideAnimations(),
+    importProvidersFrom(NgxsModule.forRoot([AppState], {
+        developmentMode: true,
+    })), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })],
 };
