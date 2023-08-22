a1022e2f38e6849c5e3265b310b6fc0c61e58e6b
Red Hood <thilakshanlk@gmail.com>
2023-08-22 19:11:10 +0530

    add firestore database

--- apps/redhood/src/app/app.config.ts
+++ apps/redhood/src/app/app.config.ts
@@ -13,6 +13,11 @@ import { AppState } from './states/app/app.state';
 import { provideServiceWorker } from '@angular/service-worker';
 import { AngularFireModule } from '@angular/fire/compat';
 import { HttpClientModule } from '@angular/common/http';
+import {MatMomentDateModule} from '@angular/material-moment-adapter'
+import {getAuth,provideAuth} from '@angular/fire/auth'
+import {getFirestore,provideFirestore} from '@angular/fire/firestore'
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
@@ -21,6 +26,18 @@ export const appConfig: ApplicationConfig = {
       NgxsModule.forRoot([AppState], {
         developmentMode: true,
     })),
+    importProvidersFrom(
+      provideFirebaseApp(()=>
+        initializeApp({
+          apiKey: "AIzaSyAc84oL9ld0_loWGakk7Ijbvir9jWx0Y5g",
+          authDomain: "redhoodmobile-fd584.firebaseapp.com",
+          projectId: "redhoodmobile-fd584",
+          storageBucket: "redhoodmobile-fd584.appspot.com",
+          messagingSenderId: "498362182800",
+          appId: "1:498362182800:web:e1d09f33660d63cda1bc15"
+        })
+      )
+    ),
     importProvidersFrom(
       AngularFireModule.initializeApp({
         apiKey: "AIzaSyAc84oL9ld0_loWGakk7Ijbvir9jWx0Y5g",
@@ -31,7 +48,10 @@ export const appConfig: ApplicationConfig = {
         appId: "1:498362182800:web:e1d09f33660d63cda1bc15"
       })
     ),
+    importProvidersFrom(provideAuth(()=>getAuth())),
+    importProvidersFrom(provideFirestore(()=>getFirestore())),
     importProvidersFrom(HttpClientModule),
+    importProvidersFrom(MatMomentDateModule),
      provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
--- apps/redhood/src/app/pages/admin/admin-grn/admin-grn.component.ts
+++ apps/redhood/src/app/pages/admin/admin-grn/admin-grn.component.ts
@@ -1,27 +1,62 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import {HttpClient} from '@angular/common/http'
-import { Observable, tap } from 'rxjs';
-
-interface User{
-  name:string;
-  email:string;
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { Firestore,addDoc,collection, collectionData} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+interface GRN{
+  date:Date;
+  customer:{
+    name:string;
+    phone:string
+  };
 }
 @Component({
   selector: 'seng41293-admin-grn',
   standalone: true,
-  imports: [CommonModule,MatButtonModule,MatCardModule],
+  imports: [CommonModule
+    ,MatButtonModule
+    ,MatCardModule
+    ,ReactiveFormsModule
+    ,MatDatepickerModule
+    ,MatInputModule
+  ],
   templateUrl: './admin-grn.component.html',
   styleUrls: ['./admin-grn.component.scss']
 })
 export class AdminGrnComponent {
   @Input({required:true}) label! :string;
+  firestore: Firestore = inject(Firestore);
 
-  users$:Observable<User[]>;
+  grns$:Observable<GRN[]>
 
-  constructor(private httpClient:HttpClient){
-    this.users$=this.httpClient.get<User[]>('https://jsonplaceholder.typicode.com/users')
+  dateCtrl=new FormControl();
+  nameCtrl=new FormControl();
+  phoneCtrl=new FormControl();
+
+  customerCtrl =new FormGroup({
+    name:this.nameCtrl,
+    phone:this.phoneCtrl,
+  })
+  grnFormGroup=new FormGroup({
+    date:this.dateCtrl,
+    customer:this.customerCtrl,
+  })
+
+  grnCollection=collection(this.firestore,'grn');
+  async OnSave(){
+    console.log(this.grnFormGroup.value);
+    const date=new Date(this.dateCtrl.value);
+    const toSave ={
+      ...this.grnFormGroup.value,
+      date,
+    }
+    await addDoc(this.grnCollection,toSave) ;
+  }
+  constructor(){
+    this.grns$=collectionData(this.grnCollection)as Observable<GRN[]>;
   }
 }
