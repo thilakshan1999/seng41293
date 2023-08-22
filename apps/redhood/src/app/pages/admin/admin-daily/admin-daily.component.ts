310ba35ce2ebf8a666bf08557a488b1cffb7ee3b
Red Hood <thilakshanlk@gmail.com>
2023-08-22 22:05:32 +0530

    add backend

---
+++ apps/redhood/src/app/pages/admin/admin-daily/admin-daily.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'seng41293-admin-daily',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin-daily.component.html',
+  styleUrls: ['./admin-daily.component.scss']
+})
+export class AdminDailyComponent {
+  
+}
--- apps/redhood/src/app/pages/admin/admin.component.ts
+++ apps/redhood/src/app/pages/admin/admin.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import { AdminGrnComponent } from './admin-grn/admin-grn.component';
 import { CommonModule } from '@angular/common';
+import { AdminDailyComponent } from './admin-daily/admin-daily.component';
 
 @Component({
   selector: 'seng41293-admin',
   standalone: true,
-  imports: [MatTabsModule,AdminGrnComponent,CommonModule],
+  imports: [MatTabsModule,AdminGrnComponent,CommonModule,AdminDailyComponent],
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
